eacd2e8c9ddbd23f8c7f585017ab1f84461ff530
Kemetnu <alhaj.reeves@lr.ehealthafrica.org>
2016-04-22 09:35:23 +0000

    update a.py

---
+++ a.py
@@ -0,0 +1,6 @@
+print "\nWelcome to interactive feedback, please answer the below question!\n "
+name =  raw_input("What is your name?\n")
+if (name=="Nathan"):
+	print ("Thank you for sharing your software knowledge with us!")
+else:
+	print "Oh so you are %s not Nathan, then pay candid attention in class!" %(name)
